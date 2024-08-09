1936ecaac2e3942ba20960872ffd69210610a38b
Dasarath G <dasarathg68@gmail.com>
2024-08-10 00:00:40 +0530

    refactor: change ui order

--- app/src/app/components/NavBar.tsx
+++ app/src/app/components/NavBar.tsx
@@ -20,6 +20,26 @@ export const NavBar = () => {
       <div className="navbar-end">
         <ConnectButton />
       </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1 ">
+          <li>
+            <details className="min-w-36">
+              <summary>Pages</summary>
+              <ul className="bg-base-100 rounded-t-none p-2">
+                <li>
+                  <a onClick={() => router.push("/member")}>Member</a>
+                </li>
+                <li>
+                  <a onClick={() => router.push("/admin")}>Admin</a>
+                </li>
+                <li>
+                  <a onClick={() => router.push("/listNFTs")}>View All NFTs</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+        </ul>
+      </div>
       <div className="dropdown dropdown-end flex">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost">
@@ -110,26 +130,7 @@ export const NavBar = () => {
           </div>
         </div>
       </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1 ">
-          <li>
-            <details className="min-w-36">
-              <summary>Pages</summary>
-              <ul className="bg-base-100 rounded-t-none p-2">
-                <li>
-                  <a onClick={() => router.push("/member")}>Member</a>
-                </li>
-                <li>
-                  <a onClick={() => router.push("/admin")}>Admin</a>
-                </li>
-                <li>
-                  <a onClick={() => router.push("/listNFTs")}>View All NFTs</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-        </ul>
-      </div>
+
       <div
         tabIndex={0}
         role="button"
