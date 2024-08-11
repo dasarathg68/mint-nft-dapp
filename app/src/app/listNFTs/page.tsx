3e4fe7d47a493b8e1bc0ffd895d8f24d544c6bf0
Dasarath G <dasarathg68@gmail.com>
2024-08-11 19:28:00 +0530

    revert:

--- app/src/app/listNFTs/page.tsx
+++ app/src/app/listNFTs/page.tsx
@@ -7,10 +7,17 @@ import { abi, address as contractAddress } from "../../constants";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
+import { useChainId } from "wagmi";
+import { chain } from "@/constants";
+import { useSwitchChain } from "wagmi";
 const listNFTs = () => {
   const [uri, setUri] = useState([]);
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [wrongChain, setWrongChain] = useState(false);
+  const { switchChain, status } = useSwitchChain();
+
+  const chainId = useChainId();
 
   const { data: tokenURIData } = useReadContract({
     abi,
@@ -18,7 +25,16 @@ const listNFTs = () => {
     functionName: "getAllURIs",
     args: [],
   });
-
+  useEffect(() => {
+    if (chainId != Number(chain)) {
+      toast.error(`Please switch chain with ID: ${chain} to use this app`);
+      setLoading(false);
+      setWrongChain(true);
+    }
+  }, []);
+  useEffect(() => {
+    if (status === "success") window.location.reload();
+  }, [status]);
   useEffect(() => {
     if (tokenURIData) {
       setUri((tokenURIData as any).filter((uri: any) => uri));
@@ -52,17 +68,31 @@ const listNFTs = () => {
 
   return (
     <div className="flex justify-center items-center min-h-screen">
-      {!loading ? (
+      {!loading && !wrongChain ? (
         <div className="mt-20 w-11/12 flex flex-col justify-center items-center">
           <h2 className="text-2xl font-semibold mb-4 text-center">
             All NFT Holders
           </h2>
           <Members members={data} />
         </div>
-      ) : (
+      ) : !wrongChain && loading ? (
         <span className="loading loading-infinity loading-lg"></span>
-      )}
+      ) : null}
 
+      {wrongChain && (
+        <div className="mt-20 w-11/12 flex flex-col justify-center items-center">
+          <button
+            className="btn btn-accent mt-4"
+            onClick={() => {
+              switchChain({
+                chainId: Number(chain) as 1 | 11155111 | 8453 | 204 | 10,
+              });
+            }}
+          >
+            Switch to correct chain
+          </button>
+        </div>
+      )}
       <ToastContainer draggable position="top-center" />
     </div>
   );
