38590d4e62039005b733c6c2fb200d130910f7f2
Dasarath G <dasarathg68@gmail.com>
2024-08-11 18:12:51 +0530

    feat: switch when wrong chain

--- app/src/app/admin/page.tsx
+++ app/src/app/admin/page.tsx
@@ -6,15 +6,22 @@ import { useReadContract, useAccount } from "wagmi";
 import { isAddress } from "ethers";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useChainId } from "wagmi";
+import { chain } from "@/constants";
+import { useSwitchChain } from "wagmi";
 
 const Admin = () => {
   const [isValid, setIsValid] = useState(true);
   const [isFileUploaded, setIsFileUploaded] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const { address: currentAddress } = useAccount();
+  const chainId = useChainId();
+  const { switchChain, status } = useSwitchChain();
+  const [wrongChain, setWrongChain] = useState(false);
+
   const { data: ownerAddress, refetch: refetchOwner } = useReadContract({
     abi,
-    address: contractAddress,
+    address: `0x${contractAddress}`,
     functionName: "owner",
     args: [],
   });
@@ -57,6 +64,7 @@ const Admin = () => {
     try {
       writeContract({
         abi,
+        // @ts-ignore: Ignore contract address type error
         address: contractAddress,
         functionName: "safeMint",
         args: [userAddress, uri],
@@ -74,6 +82,15 @@ const Admin = () => {
     }
   };
   useEffect(() => {
+    if (chainId != Number(chain)) {
+      toast.error(`Please switch chain with ID: ${chain} to use this app`);
+      setWrongChain(true);
+    }
+  }, []);
+  useEffect(() => {
+    if (status === "success") window.location.reload();
+  }, [status]);
+  useEffect(() => {
     if (isSuccess) {
       toast.success("NFT minted successfully");
       setIsSubmitting(false);
@@ -87,82 +104,102 @@ const Admin = () => {
   }, [isError]);
   return (
     <>
-      <div className=" card flex flex-col justify-center items-center min-h-screen">
-        <h1 className="text-3xl mt-20">Admin Panel</h1>
-
-        <div className="flex justify-center items-center border">
-          <form
-            className="w-full max-w-md  rounded-lg shadow-md p-5"
-            onSubmit={handleSubmit}
+      <div className="card flex flex-col justify-center items-center min-h-screen">
+        {wrongChain ? (
+          <button
+            className="btn btn-accent"
+            onClick={() => {
+              // @ts-ignore: Ignore chainId type error
+              switchChain({ chainId: Number(chain) });
+            }}
           >
-            <label
-              htmlFor="memberName"
-              className="input input-bordered flex items-center gap-2"
-            >
-              Name:
-              <input type="text" name="memberName" className="grow " />
-            </label>
-            <label htmlFor="memberImage" className="block font-medium mt-4">
-              Image:
-              <input
-                type="file"
-                onChange={handleFileInput}
-                name="memberImage"
-                className="file-input file-input-primary w-full max-w-xs mr-4 ml-4 p-2 "
-              />
-            </label>
-            <label htmlFor="memberRole" className="block font-medium mt-4">
-              Role:
-              <select
-                name="memberRole"
-                className="w-full mt-1 p-2 border  rounded focus:outline-none focus:ring-2 focus:ring-blue-400 appearance-none"
+            Switch to the correct chain
+          </button>
+        ) : (
+          <>
+            <h1 className="text-3xl mt-20">Admin Panel</h1>
+            <div className="flex justify-center items-center border">
+              <form
+                className="w-full max-w-md rounded-lg shadow-md p-5"
+                onSubmit={handleSubmit}
               >
-                <option value="member">Admin</option>
-                <option value="vice-president">Vice President</option>
-                <option value="mentor">Mentor</option>
-              </select>
-            </label>
-            <label htmlFor="memberBatch" className="block font-medium mt-4">
-              Batch:
-              <select
-                name="memberBatch"
-                className="w-full mt-1 p-2 border  rounded focus:outline-none focus:ring-2 focus:ring-blue-400  appearance-none"
-              >
-                <option value="2024">2024</option>
-                <option value="2023">2023</option>
-                <option value="2022">2022</option>
-              </select>
-            </label>
-            <label
-              htmlFor="address"
-              className="input input-bordered flex items-center gap-2 mt-4"
-            >
-              Address:
-              <input
-                type="text"
-                name="address"
-                className="grow"
-                onInput={(e) => {
-                  const inputValue = e.currentTarget.value.trim();
-                  setIsValid(isAddress(inputValue));
-                }}
-              />
-            </label>
-            {isValid ? null : (
-              <div className="text-red-800">Invalid address</div>
-            )}
-            {!isSubmitting ? (
-              <button type="submit" className="btn btn-primary mt-4 w-full">
-                Mint your NFT
-              </button>
-            ) : (
-              <button className="btn btn-secondary mt-4 w-full">
-                <span className="loading loading-spinner loading-sm text-white"></span>
-              </button>
-            )}
-          </form>
-        </div>
-        <div className="text-center mt-4">{status}</div>
+                <label
+                  htmlFor="memberName"
+                  className="input input-bordered flex items-center gap-2"
+                >
+                  Name:
+                  <input type="text" name="memberName" className="grow" />
+                </label>
+
+                <label htmlFor="memberImage" className="block font-medium mt-4">
+                  Image:
+                  <input
+                    type="file"
+                    onChange={handleFileInput}
+                    name="memberImage"
+                    className="file-input file-input-primary w-full max-w-xs mr-4 ml-4 p-2"
+                  />
+                </label>
+
+                <label htmlFor="memberRole" className="block font-medium mt-4">
+                  Role:
+                  <select
+                    name="memberRole"
+                    className="w-full mt-1 p-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400 appearance-none"
+                  >
+                    <option value="admin">Admin</option>
+                    <option value="vice-president">Vice President</option>
+                    <option value="mentor">Mentor</option>
+                  </select>
+                </label>
+
+                <label htmlFor="memberBatch" className="block font-medium mt-4">
+                  Batch:
+                  <select
+                    name="memberBatch"
+                    className="w-full mt-1 p-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400 appearance-none"
+                  >
+                    <option value="2024">2024</option>
+                    <option value="2023">2023</option>
+                    <option value="2022">2022</option>
+                  </select>
+                </label>
+
+                <label
+                  htmlFor="address"
+                  className="input input-bordered flex items-center gap-2 mt-4"
+                >
+                  Address:
+                  <input
+                    type="text"
+                    name="address"
+                    className="grow"
+                    onInput={(e) => {
+                      const inputValue = e.currentTarget.value.trim();
+                      setIsValid(isAddress(inputValue));
+                    }}
+                  />
+                </label>
+
+                {!isValid && (
+                  <div className="text-red-800">Invalid address</div>
+                )}
+
+                <button
+                  type="submit"
+                  className={`btn mt-4 w-full ${isSubmitting ? "btn-secondary" : "btn-primary"}`}
+                  disabled={isSubmitting}
+                >
+                  {isSubmitting ? (
+                    <span className="loading loading-spinner loading-sm text-white"></span>
+                  ) : (
+                    "Mint your NFT"
+                  )}
+                </button>
+              </form>
+            </div>
+          </>
+        )}
       </div>
       <ToastContainer draggable position="top-center" />
     </>
--- app/src/app/listNFTs/page.tsx
+++ app/src/app/listNFTs/page.tsx
@@ -4,21 +4,38 @@ import Members from "../components/Members";
 import { useState, useEffect } from "react";
 import { useReadContract } from "wagmi";
 import { abi, address as contractAddress } from "../../constants";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
-
+import { useChainId } from "wagmi";
+import { chain } from "@/constants";
+import { useSwitchChain } from "wagmi";
 const listNFTs = () => {
-  const { address } = useAccount();
   const [uri, setUri] = useState([]);
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [wrongChain, setWrongChain] = useState(false);
+  const { switchChain, status } = useSwitchChain();
+
+  const chainId = useChainId();
 
   const { data: tokenURIData } = useReadContract({
     abi,
+    // @ts-ignore: Ignore contract address type error
     address: contractAddress,
     functionName: "getAllURIs",
     args: [],
   });
-
+  useEffect(() => {
+    if (chainId != Number(chain)) {
+      toast.error(`Please switch chain with ID: ${chain} to use this app`);
+      setLoading(false);
+      setWrongChain(true);
+    }
+  }, []);
+  useEffect(() => {
+    if (status === "success") window.location.reload();
+  }, [status]);
   useEffect(() => {
     if (tokenURIData) {
       setUri((tokenURIData as any).filter((uri: any) => uri));
@@ -52,16 +69,31 @@ const listNFTs = () => {
 
   return (
     <div className="flex justify-center items-center min-h-screen">
-      {!loading ? (
+      {!loading && !wrongChain ? (
         <div className="mt-20 w-11/12 flex flex-col justify-center items-center">
           <h2 className="text-2xl font-semibold mb-4 text-center">
             All NFT Holders
           </h2>
           <Members members={data} />
         </div>
-      ) : (
+      ) : !wrongChain && loading ? (
         <span className="loading loading-infinity loading-lg"></span>
+      ) : null}
+
+      {wrongChain && (
+        <div className="mt-20 w-11/12 flex flex-col justify-center items-center">
+          <button
+            className="btn btn-accent mt-4"
+            onClick={() => {
+              // @ts-ignore: Ignore chainId type error
+              switchChain({ chainId: Number(chain) });
+            }}
+          >
+            Switch to correct chain
+          </button>
+        </div>
       )}
+      <ToastContainer draggable position="top-center" />
     </div>
   );
 };
